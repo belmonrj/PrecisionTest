13ed2c3605f6dbec78a238a1cf189f3e566c1034
Ron Belmont <belmonrj@gmail.com>
2017-12-10 16:20:07 -0700

    Plotting niceties etc

--- work/simple_precision.C
+++ work/simple_precision.C
@@ -34,6 +34,16 @@ void simple_precision()
   precision_test_ldb_3h222->SetMarkerColor(kBlack);
   precision_test_ldb_3h222->SetLineColor(kBlack);
   precision_test_ldb_3h222->Draw("p same");
+  TLine* line = new TLine(xmin,0,xmax,0);
+  line->SetLineStyle(2);
+  line->SetLineWidth(2);
+  TLegend* leg = new TLegend(0.18,0.68,0.38,0.88);
+  leg->AddEntry(precision_test_flt_3h222,"float","p");
+  leg->AddEntry(precision_test_dbl_3h222,"double","p");
+  leg->AddEntry(precision_test_ldb_3h222,"long double","p");
+  leg->SetTextSize(0.05);
+  line->Draw();
+  leg->Draw();
   c1->Print("test_precision_222.png");
 
   xmin = 0;
@@ -57,6 +67,58 @@ void simple_precision()
   precision_test_ldb_3hfour->SetMarkerColor(kBlack);
   precision_test_ldb_3hfour->SetLineColor(kBlack);
   precision_test_ldb_3hfour->Draw("p same");
+  line->Draw();
+  leg->Draw();
   c1->Print("test_precision_four.png");
+
+  xmin = 0;
+  xmax = 100;
+  ymin = -1e-2;
+  ymax = 1e-2;
+  delete hdummy;
+  hdummy = new TH2D("hdummy","",1,xmin,xmax,1,ymin,ymax);
+  hdummy->Draw();
+  hdummy->GetXaxis()->SetTitle("centrality");
+  hdummy->GetYaxis()->SetTitle("c_{3}{4}");
+  precision_test_flt_3hcumu->SetMarkerStyle(kOpenCircle);
+  precision_test_flt_3hcumu->SetMarkerColor(kBlack);
+  precision_test_flt_3hcumu->SetLineColor(kBlack);
+  precision_test_flt_3hcumu->Draw("p same");
+  precision_test_dbl_3hcumu->SetMarkerStyle(kOpenSquare);
+  precision_test_dbl_3hcumu->SetMarkerColor(kBlack);
+  precision_test_dbl_3hcumu->SetLineColor(kBlack);
+  precision_test_dbl_3hcumu->Draw("p same");
+  precision_test_ldb_3hcumu->SetMarkerStyle(kOpenCross);
+  precision_test_ldb_3hcumu->SetMarkerColor(kBlack);
+  precision_test_ldb_3hcumu->SetLineColor(kBlack);
+  precision_test_ldb_3hcumu->Draw("p same");
+  line->Draw();
+  leg->Draw();
+  c1->Print("test_precision_cumu.png");
+
+  xmin = 0;
+  xmax = 60;
+  ymin = -1e-4;
+  ymax = 1e-4;
+  delete hdummy;
+  hdummy = new TH2D("hdummy","",1,xmin,xmax,1,ymin,ymax);
+  hdummy->Draw();
+  hdummy->GetXaxis()->SetTitle("centrality");
+  hdummy->GetYaxis()->SetTitle("c_{3}{4}");
+  precision_test_flt_3hcumu->SetMarkerStyle(kOpenCircle);
+  precision_test_flt_3hcumu->SetMarkerColor(kBlack);
+  precision_test_flt_3hcumu->SetLineColor(kBlack);
+  precision_test_flt_3hcumu->Draw("p same");
+  precision_test_dbl_3hcumu->SetMarkerStyle(kOpenSquare);
+  precision_test_dbl_3hcumu->SetMarkerColor(kBlack);
+  precision_test_dbl_3hcumu->SetLineColor(kBlack);
+  precision_test_dbl_3hcumu->Draw("p same");
+  precision_test_ldb_3hcumu->SetMarkerStyle(kOpenCross);
+  precision_test_ldb_3hcumu->SetMarkerColor(kBlack);
+  precision_test_ldb_3hcumu->SetLineColor(kBlack);
+  precision_test_ldb_3hcumu->Draw("p same");
+  line->Draw();
+  leg->Draw();
+  c1->Print("test_precision_zumu.png");
 
 }
