0ad257dcd779102185115f26a2b5897fdb391abd
Ron Belmont <belmonrj@gmail.com>
2017-12-12 11:15:53 -0700

    Added code for the anticipated new histos with some lines to swap once they're available

--- work/simple_precision.C
+++ work/simple_precision.C
@@ -3,17 +3,7 @@ void simple_precision()
 
   TCanvas* c1 = new TCanvas("c1","");
   TFile* file = TFile::Open("precision_12452.root");
-
-  TH1D* precision_test_flt_3h222 = (TH1D*)file->Get("precision_test_flt_3h222");
-  TH1D* precision_test_flt_3hfour = (TH1D*)file->Get("precision_test_flt_3hfour");
-  TH1D* precision_test_flt_3hcumu = (TH1D*)file->Get("precision_test_flt_3hcumu");
-  TH1D* precision_test_dbl_3h222 = (TH1D*)file->Get("precision_test_dbl_3h222");
-  TH1D* precision_test_dbl_3hfour = (TH1D*)file->Get("precision_test_dbl_3hfour");
-  TH1D* precision_test_dbl_3hcumu = (TH1D*)file->Get("precision_test_dbl_3hcumu");
-  TH1D* precision_test_ldb_3h222 = (TH1D*)file->Get("precision_test_ldb_3h222");
-  TH1D* precision_test_ldb_3hfour = (TH1D*)file->Get("precision_test_ldb_3hfour");
-  TH1D* precision_test_ldb_3hcumu = (TH1D*)file->Get("precision_test_ldb_3hcumu");
-
+  // --- normal comparison to check normalization, computation issues, etc
   TProfile* tp1f_normal_two = (TProfile*)file->Get("centrality_ac_fvtxc_tracks_c32");
   TProfile* tp1f_normal_four = (TProfile*)file->Get("centrality_ac_fvtxc_tracks_c34");
   TH1D* th1d_normal_222 = tp1f_normal_two->ProjectionX("th1d_normal_222");
@@ -22,24 +12,66 @@ void simple_precision()
   th1d_normal_222->Scale(2.0);
   TH1D* th1d_normal_cumu = (TH1D*)th1d_normal_four->Clone("th1d_normal_cumu");
   th1d_normal_cumu->Add(th1d_normal_222,-1.0);
+  // --- now the precision test histos
+  // --- float
+  TH1D* precision_test_flt_3htwo = ( (TProfile*)file->Get("centrality_ac_fvtxc_tracks_c32") )->ProjectionX("h1"); // alternate to use in code development, because...
+  //TH1D* precision_test_flt_3htwo = (TH1D*)file->Get("precision_test_flt_3htwo"); // ...this is not available until RCF comes back
+  TH1D* precision_test_flt_3h222x = (TH1D*)file->Get("precision_test_flt_3h222");
+  TH1D* precision_test_flt_3hfour = (TH1D*)file->Get("precision_test_flt_3hfour");
+  TH1D* precision_test_flt_3hcumux = (TH1D*)file->Get("precision_test_flt_3hcumu");
+  // --- double
+  TH1D* precision_test_dbl_3htwo = ( (TProfile*)file->Get("centrality_ac_fvtxc_tracks_c32") )->ProjectionX("h2");
+  //TH1D* precision_test_dbl_3htwo = (TH1D*)file->Get("precision_test_dbl_3htwo");
+  TH1D* precision_test_dbl_3h222x = (TH1D*)file->Get("precision_test_dbl_3h222");
+  TH1D* precision_test_dbl_3hfour = (TH1D*)file->Get("precision_test_dbl_3hfour");
+  TH1D* precision_test_dbl_3hcumux = (TH1D*)file->Get("precision_test_dbl_3hcumu");
+  // --- long double
+  TH1D* precision_test_ldb_3htwo = ( (TProfile*)file->Get("centrality_ac_fvtxc_tracks_c32") )->ProjectionX("h3");
+  //TH1D* precision_test_ldb_3htwo = (TH1D*)file->Get("precision_test_dbl_3htwo");
+  TH1D* precision_test_ldb_3h222x = (TH1D*)file->Get("precision_test_ldb_3h222");
+  TH1D* precision_test_ldb_3hfour = (TH1D*)file->Get("precision_test_ldb_3hfour");
+  TH1D* precision_test_ldb_3hcumux = (TH1D*)file->Get("precision_test_ldb_3hcumu");
 
+  // --- need to adjust normalization before doing more calculations
   bool normalization_problem = true;
   // --- note that the normalization is correct for a single job
   // --- when aggregating single jobs, the normalization is lost,
   // --- but can be recovered by scaling by 100/nentries
   if ( normalization_problem )
     {
-      double norm = 100.0/precision_test_flt_3h222->GetEntries();
-      precision_test_flt_3h222->Scale(norm);
+      // --- need to revisit normalization once new histos are available
+      double norm = 100.0/precision_test_flt_3h222x->GetEntries();
+      //precision_test_flt_3h222->Scale(norm);
       precision_test_flt_3hfour->Scale(norm);
-      precision_test_flt_3hcumu->Scale(norm);
-      precision_test_dbl_3h222->Scale(norm);
+      //precision_test_flt_3hcumu->Scale(norm);
+      //precision_test_dbl_3h222->Scale(norm);
       precision_test_dbl_3hfour->Scale(norm);
-      precision_test_dbl_3hcumu->Scale(norm);
-      precision_test_ldb_3h222->Scale(norm);
+      //precision_test_dbl_3hcumu->Scale(norm);
+      //precision_test_ldb_3h222->Scale(norm);
       precision_test_ldb_3hfour->Scale(norm);
-      precision_test_ldb_3hcumu->Scale(norm);
+      //precision_test_ldb_3hcumu->Scale(norm);
     }
+
+  // --- do calculation here (could be averaging problems when agg'ing output)
+  // --- float
+  TH1D* precision_test_flt_3h222 = (TH1D*)precision_test_flt_3htwo->Clone("precision_test_flt_3h222");
+  precision_test_flt_3h222->Multiply(precision_test_flt_3htwo);
+  precision_test_flt_3h222->Scale(2.0);
+  TH1D* precision_test_flt_3hcumu = (TH1D*)precision_test_flt_3hfour->Clone("precision_test_flt_3hcumu");
+  precision_test_flt_3hcumu->Add(precision_test_flt_3h222,-1.0);
+  // --- double
+  TH1D* precision_test_dbl_3h222 = (TH1D*)precision_test_dbl_3htwo->Clone("precision_test_dbl_3h222");
+  precision_test_dbl_3h222->Multiply(precision_test_dbl_3htwo);
+  precision_test_dbl_3h222->Scale(2.0);
+  TH1D* precision_test_dbl_3hcumu = (TH1D*)precision_test_dbl_3hfour->Clone("precision_test_dbl_3hcumu");
+  precision_test_dbl_3hcumu->Add(precision_test_dbl_3h222,-1.0);
+  // --- long double
+  TH1D* precision_test_ldb_3h222 = (TH1D*)precision_test_ldb_3htwo->Clone("precision_test_ldb_3h222");
+  precision_test_ldb_3h222->Multiply(precision_test_ldb_3htwo);
+  precision_test_ldb_3h222->Scale(2.0);
+  TH1D* precision_test_ldb_3hcumu = (TH1D*)precision_test_ldb_3hfour->Clone("precision_test_ldb_3hcumu");
+  precision_test_ldb_3hcumu->Add(precision_test_ldb_3h222,-1.0);
+
 
   bool uncertainty_problem = true;
   for ( int i = 0; i < 100; ++i )
