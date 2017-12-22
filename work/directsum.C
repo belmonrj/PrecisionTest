94aadac4b4d2827d054ba6ed1fd1d3e64a2c1f7c
Ron Belmont <belmonrj@gmail.com>
2017-12-22 13:24:55 -0500

    Now making histos and writing to root file, added that root file

--- work/directsum.C
+++ work/directsum.C
@@ -83,24 +83,57 @@ void directsum()
       fin->Close();
     } // end loop over files
 
-  // --- loop again to do normalization
+  // --- loop again to do normalization and place output in histogram
   FILE* logfile = fopen("output.log","w");
+  TFile* outfile = TFile::Open("output.root","recreate");
+  TH1D* final_flt_222 = new TH1D("final_flt_222","",nbins,-0.5,99.5);
+  TH1D* final_flt_for = new TH1D("final_flt_for","",nbins,-0.5,99.5);
+  TH1D* final_flt_c34 = new TH1D("final_flt_c34","",nbins,-0.5,99.5);
+  TH1D* final_dbl_222 = new TH1D("final_dbl_222","",nbins,-0.5,99.5);
+  TH1D* final_dbl_for = new TH1D("final_dbl_for","",nbins,-0.5,99.5);
+  TH1D* final_dbl_c34 = new TH1D("final_dbl_c34","",nbins,-0.5,99.5);
+  TH1D* final_ldb_222 = new TH1D("final_ldb_222","",nbins,-0.5,99.5);
+  TH1D* final_ldb_for = new TH1D("final_ldb_for","",nbins,-0.5,99.5);
+  TH1D* final_ldb_c34 = new TH1D("final_ldb_c34","",nbins,-0.5,99.5);
   for ( int i = 0; i < nbins; ++i )
     {
+      // --- write unnormalized info to file
       fprintf(logfile,"i = %d flt_two = %.50e flt_for = %.50e\n",i,flt_two[i],flt_for[i]);
       fprintf(logfile,"i = %d dbl_two = %.50e dbl_for = %.50e\n",i,dbl_two[i],dbl_for[i]);
       fprintf(logfile,"i = %d ldb_two = %.50Le ldb_for = %.50Le\n",i,ldb_two[i],ldb_for[i]);
+      // --- normalize
       flt_two[i] /= flt_cnt[i];
       flt_for[i] /= flt_cnt[i];
       dbl_two[i] /= dbl_cnt[i];
       dbl_for[i] /= dbl_cnt[i];
       ldb_two[i] /= ldb_cnt[i];
       ldb_for[i] /= ldb_cnt[i];
+      // --- write normalized info to file
       fprintf(logfile,"i = %d flt_two = %.50e flt_for = %.50e\n",i,flt_two[i],flt_for[i]);
       fprintf(logfile,"i = %d dbl_two = %.50e dbl_for = %.50e\n",i,dbl_two[i],dbl_for[i]);
       fprintf(logfile,"i = %d ldb_two = %.50Le ldb_for = %.50Le\n",i,ldb_two[i],ldb_for[i]);
+      // --- calculate variables for histograms
+      float flt_222 = 2*flt_two[i]*flt_two[i];
+      float flt_c34 = flt_for[i] - flt_222;
+      double dbl_222 = 2*dbl_two[i]*dbl_two[i];
+      double dbl_c34 = dbl_for[i] - dbl_222;
+      long double ldb_222 = 2*ldb_two[i]*ldb_two[i];
+      long double ldb_c34 = ldb_for[i] - ldb_222;
+      // --- write to histograms
+      final_flt_222->SetBinContent(i+1,flt_222);
+      final_flt_for->SetBinContent(i+1,flt_for[i]);
+      final_flt_c34->SetBinContent(i+1,flt_c34);
+      final_dbl_222->SetBinContent(i+1,dbl_222);
+      final_dbl_for->SetBinContent(i+1,dbl_for[i]);
+      final_dbl_c34->SetBinContent(i+1,dbl_c34);
+      final_ldb_222->SetBinContent(i+1,ldb_222);
+      final_ldb_for->SetBinContent(i+1,ldb_for[i]);
+      final_ldb_c34->SetBinContent(i+1,ldb_c34);
     }
   fclose(logfile);
+
+  outfile->Write();
+  outfile->Close();
 
 }
 
